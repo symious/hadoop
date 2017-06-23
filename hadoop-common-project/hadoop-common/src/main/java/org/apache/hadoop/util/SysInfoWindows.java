b3e44f98e4e1727576a8751b83cce71a35e8e12b
Akira Ajisaka <aajisaka@apache.org>
2017-06-23 10:28:58 +0900

    HADOOP-12940. Fix warnings from Spotbugs in hadoop-common.
     Conflicts:
    	hadoop-common-project/hadoop-common/dev-support/findbugsExcludeFile.xml
    	hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/ECSchema.java
    
    # Conflicts:
    #	hadoop-common-project/hadoop-common/dev-support/findbugsExcludeFile.xml

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/SysInfoWindows.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/SysInfoWindows.java
@@ -170,7 +170,7 @@ public class SysInfoWindows extends SysInfo {
 
   /** {@inheritDoc} */
   @Override
-  public int getNumProcessors() {
+  public synchronized int getNumProcessors() {
     refreshIfNeeded();
     return numProcessors;
   }
@@ -197,7 +197,7 @@ public class SysInfoWindows extends SysInfo {
 
   /** {@inheritDoc} */
   @Override
-  public float getCpuUsagePercentage() {
+  public synchronized float getCpuUsagePercentage() {
     refreshIfNeeded();
     float ret = cpuUsage;
     if (ret != -1) {
@@ -218,7 +218,7 @@ public class SysInfoWindows extends SysInfo {
 
   /** {@inheritDoc} */
   @Override
-  public float getNumVCoresUsed() {
+  public synchronized float getNumVCoresUsed() {
     refreshIfNeeded();
     float ret = cpuUsage;
     if (ret != -1) {
