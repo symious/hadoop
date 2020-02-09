9c795bae0464c5e178ff9324cae17b8d15751059
Yan Xiaole <xiaole.yan@shopee.com>
2020-02-09 22:19:19 +0800

    SPDI-5115. Cherry pick SPDE-7373. Change password approach to `bcrypt` which is considered a more safe and modern way compared with `md5`

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -79,6 +79,8 @@ import org.apache.hadoop.classification.InterfaceAudience.Public;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configuration.IntegerRanges;
+import org.apache.hadoop.crypto.bcrypt.BCryptPasswordEncoder;
+import org.apache.hadoop.crypto.bcrypt.PasswordEncoder;
 import org.apache.hadoop.fs.CommonConfigurationKeys;
 import org.apache.hadoop.fs.CommonConfigurationKeysPublic;
 import org.apache.hadoop.ha.HealthCheckFailedException;
@@ -508,6 +510,7 @@ public abstract class Server {
 
   private boolean logSlowRPC = false;
   private boolean rpcPasswordAuthenticate;
+  private final PasswordEncoder passwordEncoder;
 
   /**
    * Checks if LogSlowRPC is set true.
@@ -2830,7 +2833,7 @@ public abstract class Server {
           if (serverRpcPassword == null) {
             throw new IOException("No rpcPassword record on server side for user: " + userName);
           }
-          if (rpcPassword == null || !serverRpcPassword.equals(MD5Hash.digest(rpcPassword).toString())) {
+          if (rpcPassword == null || !passwordEncoder.matches(rpcPassword, serverRpcPassword)) {
             throw new IOException("Rpc Authentication failed for user: " + userName);
           }
         }
@@ -3189,6 +3192,7 @@ public abstract class Server {
     this.setLogSlowRPC(conf.getBoolean(
         CommonConfigurationKeysPublic.IPC_SERVER_LOG_SLOW_RPC,
         CommonConfigurationKeysPublic.IPC_SERVER_LOG_SLOW_RPC_DEFAULT));
+    this.passwordEncoder = new BCryptPasswordEncoder();
 
     // Create the responder here
     responder = new Responder();
