2e15ce652a4dad950570ec7ea77229eee1b175ca
yiyang.zhou <yiyang.zhou@shopee.com>
2020-02-25 23:47:14 +0800

    SPDI-103. Only check SDICredential for AuthMethod.SIMPLE

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -2378,7 +2378,7 @@ public abstract class Server {
           }
         }
       }
-      if (isRpcPasswordAuthenticate() && !ignoreSDIAuthenticate) {
+      if (authMethod == AuthMethod.SIMPLE && isRpcPasswordAuthenticate() && !ignoreSDIAuthenticate) {
         authenticateConnection();
       }
       authorizeConnection();
