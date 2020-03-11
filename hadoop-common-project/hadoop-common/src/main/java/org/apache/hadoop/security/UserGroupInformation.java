e6841fa95a8c9ef765cac122ec2f1b6c942c59b8
yiyang.zhou <yiyang.zhou@shopee.com>
2020-03-11 17:20:19 +0800

    SPDI-217. Add password in BestUgi

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/UserGroupInformation.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/UserGroupInformation.java
@@ -607,7 +607,7 @@ public class UserGroupInformation {
     } else if (user == null) {
       return getCurrentUser();
     } else {
-      return createRemoteUser(user);
+      return createRemoteUser(user, SdiCredentialsUtil.getSdiUserRpcPassword());
     }    
   }
 
