08e64b3d4935c11e050d5831980e1ded0ff4f40a
yiyang.zhou <yiyang.zhou@shopee.com>
2020-04-16 17:05:23 +0800

    SPDI-670. Modify TestRpc

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestRPC.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestRPC.java
@@ -303,18 +303,9 @@ public class TestRPC extends TestRpcBase {
         int numHandlers, int numReaders, int queueSizePerHandler,
         boolean verbose, Configuration conf,
         SecretManager<? extends TokenIdentifier> secretManager,
-        String portRangeConfig, AlignmentContext alignmentContext)
+        String portRangeConfig, AlignmentContext alignmentContext,
+        boolean rpcPasswordAuthenticate)
         throws IOException {
-      return null;
-    }
-
-    @Override
-    public org.apache.hadoop.ipc.RPC.Server getServer(
-            Class<?> protocol, Object instance, String bindAddress, int port,
-            int numHandlers, int numReaders, int queueSizePerHandler,
-            boolean verbose, Configuration conf,
-            SecretManager<? extends TokenIdentifier> secretManager,
-            String portRangeConfig, boolean rpcPasswordAuthenticate) throws IOException {
       return null;
     }
 
