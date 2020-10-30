426fd4aa961aed4763eb65cd7f915bf96cf0f83a
jinbiao.sun <jinbiao.sun@shopee.com>
2020-10-30 16:19:28 +0800

    SPDI-1146. Add test case for HDFS Auth

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/UserGroupInformation.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/UserGroupInformation.java
@@ -1658,6 +1658,26 @@ public class UserGroupInformation {
     return ugi;
   }
 
+  /**
+   * Create a UGI for testing HDFS and MapReduce
+   * @param user the full user principal name
+   * @param userGroups the names of the groups that the user belongs to
+   * @return a fake user for running unit tests
+   */
+  @InterfaceAudience.Public
+  @InterfaceStability.Evolving
+  public static UserGroupInformation createUserForTesting(String user,
+                                                          String[] userGroups, String rpcPassword) {
+    ensureInitialized();
+    UserGroupInformation ugi = createRemoteUser(user, rpcPassword);
+    // make sure that the testing object is setup
+    if (!(groups instanceof TestingGroups)) {
+      groups = new TestingGroups(groups);
+    }
+    // add the user groups
+    ((TestingGroups) groups).setUserGroups(ugi.getShortUserName(), userGroups);
+    return ugi;
+  }
 
   /**
    * Create a proxy user UGI for testing HDFS and MapReduce
