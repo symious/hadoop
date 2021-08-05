a0d5763c6c9d6a6430bdadb9ea402439cdc96c0a
Yan Xiaole <xiaole.yan@shopee.com>
2021-08-05 12:19:04 +0800

    SPDI-16065. Correct client's trash dest message while removing data to trash.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/TrashPolicyDefault.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/TrashPolicyDefault.java
@@ -165,6 +165,9 @@ public class TrashPolicyDefault extends TrashPolicy {
         // move to current trash
         fs.rename(path, trashPath,
             Rename.TO_TRASH);
+
+        // SPDI-16065. Only correct the client output, since the actual control is at NN side.
+        trashPath = correctTrashPath(path, trashPath);
         LOG.info("Moved: '" + path + "' to trash at: " + trashPath);
         return true;
       } catch (IOException e) {
@@ -173,6 +176,16 @@ public class TrashPolicyDefault extends TrashPolicy {
     }
     throw (IOException)
       new IOException("Failed to move to trash: " + path).initCause(cause);
+  }
+
+  private Path correctTrashPath(Path src, Path dst) {
+    String[] split = dst.toString().split("/");
+    // Old format is /user/$USER/.Trash/Current/abcxyz....
+    String user = split[2];
+
+    Path trashRoot = new Path("/Trash", user);
+    Path trashCurrent = new Path(trashRoot, "Current");
+    return Path.mergePaths(trashCurrent, src);
   }
 
   @SuppressWarnings("deprecation")
