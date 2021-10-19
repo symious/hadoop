20fa34142a7c965f2686a5027335b6bbe183fcbd
Yiyang Zhou <yiyang.zhou@shopee.com>
2021-10-19 14:40:01 +0800

    SPDI-31563. Backport SPDI-21455. Check md5 before loading shadow file to Hadoop 3.2.0

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ShadowFileRpcPasswordMapping.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ShadowFileRpcPasswordMapping.java
@@ -23,17 +23,27 @@ import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.CommonConfigurationKeys;
+import org.apache.hadoop.io.MD5Hash;
+import org.apache.hadoop.metrics2.annotation.Metric;
+import org.apache.hadoop.metrics2.annotation.Metrics;
+import org.apache.hadoop.metrics2.lib.DefaultMetricsSystem;
+import org.apache.hadoop.metrics2.lib.MetricsRegistry;
+import org.apache.hadoop.metrics2.lib.MutableGaugeLong;
+import org.apache.hadoop.metrics2.lib.MutableRate;
 import org.apache.hadoop.util.Time;
+import org.apache.hadoop.util.hash.MD5FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -48,20 +58,44 @@ public class ShadowFileRpcPasswordMapping extends Configured
   protected static final Logger LOG =
       LoggerFactory.getLogger(ShadowFileRpcPasswordMapping.class);
 
+  /** Metrics to track shadow file activity */
+  static ShadowFileMetrics metrics = ShadowFileMetrics.create();
   private String shadowFile = CommonConfigurationKeys.
       HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_DEFAULT;
   private static final String EMPTY_PASSWORD = null;
+  private boolean checksumEnabled = CommonConfigurationKeys.
+      HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_CHECKSUM_ENABLED_DEFAULT;
 
   private long cacheTimeout =
-          CommonConfigurationKeys.
-              HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_CACHE_SEC_DEFAULT * 1000;
-  private volatile long lastRefreshTime = -1L;
+      CommonConfigurationKeys.
+          HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_CACHE_SEC_DEFAULT * 1000;
+  private AtomicLong lastRefreshTime = new AtomicLong(-1L);
+  private volatile boolean isStartup = true;
 
   private AtomicReference<ConcurrentHashMap<String, RpcPasswordAndBypass>>
       cacheRef = new AtomicReference<>();
 
-  private IOException IllegalShadowLineException(String line) {
-    return new IOException("Illegal shadow line: " + line);
+  /**
+   * ShadowFileMetrics maintains shadow file related statistics.
+   */
+  @Metrics(about="shadow file related metrics", context="shadowFile")
+  static class ShadowFileMetrics {
+    final MetricsRegistry registry = new MetricsRegistry("ShadowFileMetrics");
+
+    @Metric("Rate of successful shadow file refresh and latency (milliseconds)")
+    MutableRate refreshSuccess;
+    @Metric("Rate of failed shadow file refresh and latency (milliseconds)")
+    MutableRate refreshFailure;
+    @Metric("Refresh total since startup")
+    private MutableGaugeLong refreshTotal;
+    @Metric("Refresh failures since startup")
+    private MutableGaugeLong refreshFailuresTotal;
+    @Metric("Process line failure since startup")
+    private MutableGaugeLong processLineFailuresTotal;
+
+    static ShadowFileMetrics create() {
+      return DefaultMetricsSystem.instance().register(new ShadowFileMetrics());
+    }
   }
 
   @Override
@@ -73,6 +107,12 @@ public class ShadowFileRpcPasswordMapping extends Configured
               HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE,
           CommonConfigurationKeys.
               HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_DEFAULT);
+      checksumEnabled = conf.getBoolean(
+          CommonConfigurationKeys.
+              HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_CHECKSUM_ENABLED,
+          CommonConfigurationKeys.
+              HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_CHECKSUM_ENABLED_DEFAULT
+      );
       cacheTimeout = conf.getLong(
           CommonConfigurationKeys.
               HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_CACHE_SEC,
@@ -97,14 +137,13 @@ public class ShadowFileRpcPasswordMapping extends Configured
       if (isTimeout()) {
         cacheRefresh(false);
       }
-      if (!cacheRef.get().containsKey(userName)){
-        return null;
-      }
-      return cacheRef.get().get(userName).getRpcPassword();
-    }catch (IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
-    return null;
+    if (!cacheRef.get().containsKey(userName)) {
+      return null;
+    }
+    return cacheRef.get().get(userName).getRpcPassword();
   }
 
   @Override
@@ -116,26 +155,35 @@ public class ShadowFileRpcPasswordMapping extends Configured
       if (isTimeout()) {
         cacheRefresh(false);
       }
-      if (!cacheRef.get().containsKey(user)) {
-        return false;
-      }
-      return cacheRef.get().get(user).isBypass();
     } catch (IOException e) {
       e.printStackTrace();
     }
-    return false;
+    if (!cacheRef.get().containsKey(user)) {
+      return false;
+    }
+    return cacheRef.get().get(user).isBypass();
   }
 
-  @Override
-  public synchronized void cacheRefresh(boolean force) throws IOException {
-    if (!force){
+  public void cacheRefresh(boolean force) throws IOException {
+    long start = Time.now();
+    if (!force) {
       // If not force refresh, check the timeout again
       if (!isTimeout())
         return;
     }
+    metrics.refreshTotal.incr();
     BufferedReader br = null;
     ConcurrentHashMap<String, RpcPasswordAndBypass> updateCache =
         new ConcurrentHashMap<>();
+
+    MD5Hash md5Hash = null;
+    if (checksumEnabled && !isStartup) {
+      md5Hash = checksum();
+      if (md5Hash == null) {
+        refreshFailure("First round checksum not match.", start);
+      }
+    }
+
     try {
       FileInputStream file = new FileInputStream(shadowFile);
       Reader fr = new InputStreamReader(file, StandardCharsets.UTF_8);
@@ -146,22 +194,55 @@ public class ShadowFileRpcPasswordMapping extends Configured
         try {
           processRow(updateCache, line);
         } catch (IllegalShadowLineException e) {
-          LOG.warn("unable to process shadow line: {}", line, e);
+          metrics.processLineFailuresTotal.incr();
+          LOG.error("Unable to process shadow line: " + line, start);
         }
       }
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("Refreshed " + updateCache.size() +
-            " records from shadowFile.");
-      }
-      cacheRef.set(updateCache);
-      lastRefreshTime = Time.now();
     } catch (IOException e) {
-      e.printStackTrace();
+      throw e;
     } finally {
       if (br != null) {
         br.close();
       }
     }
+
+    if (checksumEnabled && !isStartup) {
+      MD5Hash fileHash = MD5FileUtils.computeMd5ForFile(new File(shadowFile));
+      if (md5Hash != null && !md5Hash.equals(fileHash)) {
+        refreshFailure("Second round checksum not match", start);
+      }
+    }
+    if (updateCache.isEmpty()) {
+      refreshFailure("New shadowFile is empty", start);
+    }
+    cacheRef.set(updateCache);
+    lastRefreshTime.set(Time.now());
+    LOG.info("Refreshed " + updateCache.size() + " records from shadowFile.");
+    if (isStartup) {
+      isStartup = false;
+    }
+    metrics.refreshSuccess.add(Time.now() - start);
+  }
+
+  private MD5Hash checksum() {
+    MD5Hash fileHash, storedHash;
+    MD5Hash result = null;
+    try {
+      fileHash = MD5FileUtils.computeMd5ForFile(new File(shadowFile));
+      storedHash = MD5FileUtils.readStoredMd5ForFile(
+          new File(shadowFile));
+      if (storedHash == null) {
+        LOG.error("MD5 File not exists: " +
+            MD5FileUtils.getDigestFileForFile(new File(shadowFile)));
+      }
+      if (!fileHash.equals(storedHash)) {
+        return null;
+      }
+      result = fileHash;
+    } catch (IOException e) {
+      LOG.error("Error checksum: " + e.getMessage());
+    }
+    return result;
   }
 
   private void processRow(ConcurrentHashMap<String, RpcPasswordAndBypass> cache,
@@ -179,7 +260,12 @@ public class ShadowFileRpcPasswordMapping extends Configured
   }
 
   private boolean isTimeout() {
-    return Time.now() - lastRefreshTime > cacheTimeout;
+    return Time.now() - lastRefreshTime.get() > cacheTimeout;
+  }
+
+  @VisibleForTesting
+  public long getLastRefreshTime() {
+    return lastRefreshTime.get();
   }
 
   private static class IllegalShadowLineException extends IOException {
@@ -198,5 +284,17 @@ public class ShadowFileRpcPasswordMapping extends Configured
       sb.append(super.getMessage());
       return sb.toString();
     }
+  }
+
+  private void refreshSuccess(long start) {
+
+  }
+
+  private void refreshFailure(String reason, long start)
+      throws ShadowFileException {
+    lastRefreshTime.set(Time.now());
+    metrics.refreshFailure.add(Time.now() - start);
+    metrics.refreshFailuresTotal.incr();
+    throw new ShadowFileException(reason);
   }
 }
---
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/security/TestShadowFilePasswordMapping.java
@@ -0,0 +1,138 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.security;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.CommonConfigurationKeys;
+import org.apache.hadoop.io.MD5Hash;
+import org.apache.hadoop.test.GenericTestUtils;
+import org.apache.hadoop.util.hash.MD5FileUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+
+public class TestShadowFilePasswordMapping {
+  private static final Logger TESTLOG =
+      LoggerFactory.getLogger(TestShadowFilePasswordMapping.class);
+
+  private final GenericTestUtils.LogCapturer shadowFileLog =
+      GenericTestUtils.LogCapturer.captureLogs(
+          ShadowFileRpcPasswordMapping.LOG);
+
+  private final static String TEST_SHADOW_FILE_1 = "shadow1";
+  private final static String TEST_SHADOW_FILE_2 = "shadow2";
+
+  private final static long BACKOFF_MS = 10L;
+
+  @Test
+  public void testCacheRefresh() throws IOException {
+    ShadowFileRpcPasswordMapping mapping = new ShadowFileRpcPasswordMapping();
+    Configuration conf = new Configuration();
+    ClassLoader classLoader = getClass().getClassLoader();
+
+    File shadowFile1 = new File(classLoader.getResource(TEST_SHADOW_FILE_1).getFile());
+    conf.set(CommonConfigurationKeys.
+        HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE, shadowFile1.getAbsolutePath());
+    System.out.println(shadowFile1.getAbsolutePath());
+    mapping.setConf(conf);
+
+    mapping.cacheRefresh(true);
+
+    assertEquals(mapping.getRpcPassword("a"), "aaaShadow1");
+    assertNull(mapping.getRpcPassword("b"));
+    assertEquals(mapping.getRpcPassword("c"), "cccShadow1");
+
+    File shadowFile2 = new File(classLoader.getResource(TEST_SHADOW_FILE_2).getFile());
+    conf.set(CommonConfigurationKeys.
+        HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE, shadowFile2.getAbsolutePath());
+    System.out.println(shadowFile1.getAbsolutePath());
+    mapping.setConf(conf);
+
+    mapping.cacheRefresh(true);
+
+    assertEquals(mapping.getRpcPassword("a"), "aaaShadow2");
+    assertNull(mapping.getRpcPassword("b"));
+    assertEquals(mapping.getRpcPassword("c"), "cccShadow2");
+
+  }
+
+  @Test
+  public void testChecksum() throws IOException {
+    ShadowFileRpcPasswordMapping mapping = new ShadowFileRpcPasswordMapping();
+    ClassLoader classLoader = getClass().getClassLoader();
+    File shadowFile1 = new File(classLoader.getResource(TEST_SHADOW_FILE_1).getFile());
+    File shadowFile2 = new File(classLoader.getResource(TEST_SHADOW_FILE_2).getFile());
+    Configuration conf = new Configuration();
+    conf.setBoolean(CommonConfigurationKeys.
+        HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE_CHECKSUM_ENABLED, true);
+    conf.set(CommonConfigurationKeys.
+        HADOOP_SECURITY_RPC_PASSWORD_SHADOW_FILE, shadowFile1.getAbsolutePath());
+    mapping.setConf(conf);
+
+    try {
+      // Clean shadow1.md5
+      File md5File = MD5FileUtils.getDigestFileForFile(shadowFile1);
+      if (md5File.exists()) {
+        MD5FileUtils.getDigestFileForFile(shadowFile1).delete();
+      }
+      // Ignore md5 when start up
+      mapping.cacheRefresh(true);
+
+      // shadow1.md5 not exists
+      try {
+        mapping.cacheRefresh(true);
+        fail("Should fail since md5 not exists.");
+      } catch (Exception e) {
+        assertTrue(e instanceof ShadowFileException);
+      }
+
+
+      // Create shadow1.md5
+      MD5Hash md5Hash = MD5FileUtils.computeMd5ForFile(shadowFile1);
+      MD5FileUtils.saveMD5File(shadowFile1, md5Hash);
+      mapping.cacheRefresh(true);
+
+      // Create shadow1.md5 based on shadow2
+      MD5Hash md5Hash2 = MD5FileUtils.computeMd5ForFile(shadowFile2);
+      MD5FileUtils.saveMD5File(shadowFile1, md5Hash2);
+      try {
+        mapping.cacheRefresh(true);
+        fail("Should fail since md5 not match.");
+      } catch (Exception e) {
+        assertTrue(e instanceof ShadowFileException);
+      }
+    } finally {
+      // Delete shadow1.md5
+      MD5FileUtils.getDigestFileForFile(shadowFile1).delete();
+    }
+  }
+
+}
+
+
