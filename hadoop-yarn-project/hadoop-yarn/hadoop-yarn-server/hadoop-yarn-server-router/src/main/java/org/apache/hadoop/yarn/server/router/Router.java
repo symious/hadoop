70c4fddbd9647d21644fbe254330066a4cab8475
Luo Yuan <luoyuan@shopee.com>
2021-11-10 11:34:46 +0800

    SPDI-23379. Add version and process startup time on Router page

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-router/src/main/java/org/apache/hadoop/yarn/server/router/Router.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-router/src/main/java/org/apache/hadoop/yarn/server/router/Router.java
@@ -29,6 +29,7 @@ import org.apache.hadoop.service.CompositeService;
 import org.apache.hadoop.util.JvmPauseMonitor;
 import org.apache.hadoop.util.ShutdownHookManager;
 import org.apache.hadoop.util.StringUtils;
+import org.apache.hadoop.util.Time;
 import org.apache.hadoop.yarn.YarnUncaughtExceptionHandler;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnRuntimeException;
@@ -89,6 +90,8 @@ public class Router extends CompositeService {
   public static final int SHUTDOWN_HOOK_PRIORITY = 30;
 
   private static final String METRICS_NAME = "Router";
+
+  private static long routerStartupTime = Time.monotonicNow();
 
   public Router() {
     super(Router.class.getName());
@@ -183,6 +186,10 @@ public class Router extends CompositeService {
     Builder<Object> builder =
         WebApps.$for("cluster", null, null, "ws").with(conf).at(webAppAddress);
     webApp = builder.start(new RouterWebApp(this));
+  }
+
+  public static long getRouterStartupTime() {
+    return routerStartupTime;
   }
 
   public static void main(String[] argv) {
