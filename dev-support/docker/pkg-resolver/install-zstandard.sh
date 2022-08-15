9545f15ad9367c667d554bccb974f7f9109cc91b
zengqiang.xu <zengqiang.xu@shopee.com>
2022-08-15 11:03:24 +0800

    SPDI-53769. Release new hadoop version includes all jar files and native lib

---
+++ dev-support/docker/pkg-resolver/install-zstandard.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# hadolint ignore=DL3003
+mkdir -p /opt/zstd /tmp/zstd &&
+  curl -L -s -S https://github.com/facebook/zstd/archive/refs/tags/v1.5.0.tar.gz -o /tmp/zstd/v1.5.0.tar.gz &&
+  tar xzf /tmp/zstd/v1.5.0.tar.gz --strip-components 1 -C /opt/zstd &&
+  cd /opt/zstd || exit &&
+  make BUILD_DIR=libzstd "-j$(nproc)" &&
+  make install &&
+  cd /root || exit
+
