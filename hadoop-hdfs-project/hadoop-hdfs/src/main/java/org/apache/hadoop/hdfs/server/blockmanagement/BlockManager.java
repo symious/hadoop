103d5148290512ce810c84453e785411719dc232
zengqiang.xu <zengqiang.xu@shopee.com>
2022-11-18 14:25:03 +0800

    SPDI-65724. Backport HDFS-16138. BlockReportProcessingThread exit doesnt print the acutal stack

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
@@ -5215,6 +5215,7 @@ public class BlockManager implements BlockStatsMXBean {
       try {
         processQueue();
       } catch (Throwable t) {
+        LOG.error("Error while processing the block report, terminating the process", t);
         ExitUtil.terminate(1,
             getName() + " encountered fatal exception: " + t);
       }
