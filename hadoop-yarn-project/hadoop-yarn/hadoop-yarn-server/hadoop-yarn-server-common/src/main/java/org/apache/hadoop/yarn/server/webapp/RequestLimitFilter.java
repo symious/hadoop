bef3b6c22baa0f804f08e04c5f1e8dd975beec85
Zhanqi Cai <zhanqi.cai@shopee.com>
2023-01-05 07:05:18 +0000

    SPDI-73055. Limit the request ratio by request client IP

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/webapp/RequestLimitFilter.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/webapp/RequestLimitFilter.java
@@ -27,20 +27,30 @@ public class RequestLimitFilter implements Filter {
 
   private int maxRequestsPerMinutePerAPI;
 
+  private int maxRequestsPerMinutePerIp;
+
   private int maxRequestsPerMinute;
   private static String MAX_REQUESTS_PER_MINUTE_PER_API = "max-requests-per-minute-per-api";
+
+  private static String MAX_REQUESTS_PER_MINUTE_PER_IP = "max-requests-per-minute-per-ip";
 
   private static String MAX_REQUESTS_PER_MINUTE = "max-requests-per-minute";
   private static String ALLOW_USERS = "allow-users";
 
   private static String WHITE_USER_FROM_REQUEST = "ALLOW-USER";
+
+  private static String GENERAL_SKIP_IPS = "general-skip-ips";
   private Map<String, RequestLimit> requestsMap;
+
+  private Map<String, Integer> reqIpsMap;
 
   private ArrayBlockingQueue<RequestLimit> requestsQueue;
 
-  private static String[] ignorePathPrefix = {"/static/", "/prom"};
+  private static String[] ignorePathPrefix = {"/static/", "/prom", "/jmx"};
 
   private Set<String> users;
+
+  private Set<String> generalSkipIPs;
 
   private static int ONE_MINUTE = 60 * 1000;
 
@@ -56,11 +66,22 @@ public class RequestLimitFilter implements Filter {
     if (!StringUtils.isNullOrEmpty(totalReqPerMinute)) {
       maxRequestsPerMinute = Integer.valueOf(totalReqPerMinute);
     }
+    maxRequestsPerMinutePerIp = 10;
+    String maxReqPerMinutePerIp = filterConfig.getInitParameter(MAX_REQUESTS_PER_MINUTE_PER_IP);
+    if (!StringUtils.isNullOrEmpty(maxReqPerMinutePerIp)) {
+      maxRequestsPerMinutePerIp = Integer.valueOf(maxReqPerMinutePerIp);
+    }
     requestsMap = new ConcurrentHashMap<>();
+    reqIpsMap = new ConcurrentHashMap<>();
     users = new HashSet<>();
     String whiteUsers = filterConfig.getInitParameter(ALLOW_USERS);
     if (!StringUtils.isNullOrEmpty(whiteUsers)) {
       users.addAll(Arrays.asList(whiteUsers.split(",")));
+    }
+    generalSkipIPs = new HashSet<>();
+    String skipIPs = filterConfig.getInitParameter(GENERAL_SKIP_IPS);
+    if (!StringUtils.isNullOrEmpty(skipIPs)) {
+      generalSkipIPs.addAll(Arrays.asList(skipIPs.split(",")));
     }
     requestsQueue = new ArrayBlockingQueue<>(maxRequestsPerMinute);
   }
@@ -86,10 +107,24 @@ public class RequestLimitFilter implements Filter {
               return;
             }
 
+            String ipAddress = getIpFromHttp(request);
+
+            // Per Ip Request Limit
+            Integer count = reqIpsMap.get(ipAddress);
+            if (null == count) {
+              count = 0;
+            }
+
+            if (count > maxRequestsPerMinutePerIp) {
+              response.getWriter()
+                  .write("Current request count exceeded the Total Allowed Count for Per IP");
+              return;
+            }
+
             RequestLimit requestLimit = requestsMap.get(requestPath);
             if (null == requestLimit) {
               // record per api
-              requestLimit = new RequestLimit(requestPath);
+              requestLimit = new RequestLimit(requestPath, ipAddress);
               requestsMap.put(requestPath, requestLimit);
             }
 
@@ -100,16 +135,31 @@ public class RequestLimitFilter implements Filter {
             }
 
             // add to queue
-            requestsQueue.put(new RequestLimit(requestPath));
+            requestsQueue.put(new RequestLimit(requestPath, ipAddress));
             requestLimit.requestNum++;
+            if (!generalSkipIPs.contains(ipAddress)) {
+              reqIpsMap.put(ipAddress, ++count);
+            }
+
+            LOG.info("The total request count within one minute:" + requestsQueue.size());
 
             // Log
             if (LOG.isDebugEnabled()) {
               for (String path : requestsMap.keySet()) {
                 LOG.debug("request path:" + path + ", count:" + requestsMap.get(path).requestNum);
               }
+
+              for (String tmpIpAddress : reqIpsMap.keySet()) {
+                LOG.debug("request ip:" + tmpIpAddress + ", count:" + reqIpsMap.get(tmpIpAddress));
+              }
+
+              for (String skipIp : generalSkipIPs) {
+                LOG.debug("General Skip IP:" + skipIp);
+              }
+
               LOG.debug("total request count:" + requestsQueue.size());
               LOG.debug("Per Minute Limit for per request:" + maxRequestsPerMinutePerAPI);
+              LOG.debug("Per Minute Limit for per Ip:" + maxRequestsPerMinutePerIp);
               LOG.debug("Per Minute Limit for total request:" + maxRequestsPerMinute);
             }
           } catch (Exception e) {
@@ -120,6 +170,47 @@ public class RequestLimitFilter implements Filter {
     }
     filterChain.doFilter(servletRequest, servletResponse);
   }
+
+  private String getIpFromHttp(HttpServletRequest request) {
+    String ip = null;
+    // X-Forwarded-For:Squid proxy
+    String ipAddresses = request.getHeader("X-Forwarded-For");
+    if (ipAddresses == null || ipAddresses.length() == 0 ||
+        "unknown".equalsIgnoreCase(ipAddresses)) {
+      // Proxy-Client-IP:apache proxy
+      ipAddresses = request.getHeader("Proxy-Client-IP");
+    }
+    if (ipAddresses == null || ipAddresses.length() == 0 ||
+        "unknown".equalsIgnoreCase(ipAddresses)) {
+      // WL-Proxy-Client-IP:weblogic proxy
+      ipAddresses = request.getHeader("WL-Proxy-Client-IP");
+    }
+    if (ipAddresses == null || ipAddresses.length() == 0 ||
+        "unknown".equalsIgnoreCase(ipAddresses)) {
+      // HTTP_CLIENT_IP proxy
+      ipAddresses = request.getHeader("HTTP_CLIENT_IP");
+    }
+    if (ipAddresses == null || ipAddresses.length() == 0 ||
+        "unknown".equalsIgnoreCase(ipAddresses)) {
+      // X-Real-IP:nginx proxy
+      ipAddresses = request.getHeader("X-Real-IP");
+    }
+
+    if (ipAddresses != null && ipAddresses.length() != 0) {
+      ip = ipAddresses.split(",")[0];
+    }
+
+    // request.getRemoteAddr()
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+      ip = request.getRemoteAddr();
+    }
+
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("request ip:" + ip);
+    }
+    return ip;
+  }
+
 
   private void cleanOverTimeData() {
     long currentTime = System.currentTimeMillis();
@@ -136,6 +227,16 @@ public class RequestLimitFilter implements Filter {
           req.requestNum--;
           if (req.requestNum <= 0) {
             requestsMap.remove(requestLimit.reqPath);
+          }
+        }
+
+        Integer count = reqIpsMap.get(requestLimit.ip);
+        if (null != count) {
+          count--;
+          if (count <= 0) {
+            reqIpsMap.remove(requestLimit.ip);
+          } else {
+            reqIpsMap.put(requestLimit.ip, count);
           }
         }
       }
@@ -169,9 +270,12 @@ public class RequestLimitFilter implements Filter {
 
     String reqPath;
 
-    RequestLimit(String requestPath) {
+    String ip;
+
+    RequestLimit(String requestPath, String ipAddress) {
       reqPath = requestPath;
       requestNum = 0;
+      ip = ipAddress;
       lastLimitRequestTime = System.currentTimeMillis();
     }
   }
