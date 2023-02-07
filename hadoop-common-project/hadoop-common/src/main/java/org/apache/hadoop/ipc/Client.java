f8648cce71cc6969b5ea5e4eff22b709a12fa92c
zengqiang.xu <zengqiang.xu@shopee.com>
2023-02-07 19:31:20 +0800

    SPDI-76306. There are some stacked calls in ObserverNameNode

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
@@ -1440,10 +1440,12 @@ public class Client implements AutoCloseable {
     final Connection connection = getConnection(remoteId, call, serviceClass,
         fallbackToSimpleAuth);
 
+    boolean success = false;
     try {
       checkAsyncCall();
       try {
         connection.sendRpcRequest(call);                 // send the rpc request
+        success = true;
       } catch (RejectedExecutionException e) {
         throw new IOException("connection has been closed", e);
       } catch (InterruptedException ie) {
@@ -1458,6 +1460,10 @@ public class Client implements AutoCloseable {
         releaseAsyncCall();
       }
       throw e;
+    } finally {
+      if (!success) {
+        connection.calls.remove(call.id);
+      }
     }
 
     if (isAsynchronousMode()) {
