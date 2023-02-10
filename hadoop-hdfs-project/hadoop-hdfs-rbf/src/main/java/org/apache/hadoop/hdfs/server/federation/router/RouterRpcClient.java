230e8713bb05c4b6d5b9f239f1bbf2a1491d3834
Fermi Fei <fermi.fei@shopee.com>
2023-02-10 10:56:29 +0000

    SPDI-71494. Namenode status was wrong on Routers

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/RouterRpcClient.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/RouterRpcClient.java
@@ -590,8 +590,10 @@ public class RouterRpcClient {
         final Object proxy = client.getProxy();
 
         ret = invoke(nsId, 0, method, proxy, params);
-        if (failover && protocol == ClientProtocol.class) {
-          // Success on alternate server, update
+        if (failover && protocol == ClientProtocol.class
+            && (!isRead(method) || !observerReadEnabled)) {
+          // Success on WRITE or without the Observer feature, update
+          // Don't update on READ because maybe it is an Observer. Let HeartbeatService update it in this case.
           namenodeResolver.updateActiveNamenode(nsId, client.getAddress());
         }
         if (this.rpcMonitor != null) {
