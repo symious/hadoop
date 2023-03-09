b609451a7c611c8b86656ddc360eebd72aebefe8
Jinbiao Sun <jinbiao.sun@shopee.com>
2023-03-09 09:06:10 +0000

    SPDI-79813. Revert symlink change

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
@@ -4186,7 +4186,7 @@ public abstract class FileSystem extends Configured
 
   // Symlinks are temporarily disabled - see HADOOP-10020 and HADOOP-10052
   // Updates are enabled by default on the client side
-  private static boolean symlinksEnabled = true;
+  private static boolean symlinksEnabled = false;
 
   @VisibleForTesting
   public static boolean areSymlinksEnabled() {
