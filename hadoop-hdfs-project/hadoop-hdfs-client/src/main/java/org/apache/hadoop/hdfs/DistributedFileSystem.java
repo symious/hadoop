3e7a562872aec888794b4de56f062e4ffa7fbc50
jinbiao.sun <jinbiao.sun@shopee.com>
2023-03-09 17:18:28 +0800

    SPDI-79813. Revert SPDI-78362 Update DistributedFileSystem#createSymlink() logic

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DistributedFileSystem.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DistributedFileSystem.java
@@ -1616,7 +1616,7 @@ public class DistributedFileSystem extends FileSystem
     new FileSystemLinkResolver<Void>() {
       @Override
       public Void doCall(final Path p) throws IOException {
-        dfs.createSymlink(getPathName(target), getPathName(p), createParent);
+        dfs.createSymlink(target.toString(), getPathName(p), createParent);
         return null;
       }
       @Override
