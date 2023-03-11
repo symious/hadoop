c11cd772985cdea66c4850da7089f50e321dc032
Haiyang <haiyang.hu@shopee.com>
2023-03-11 16:19:31 +0800

    SPDI-80652. Fix LocalFilesytem call FileStatus#isFile() exception

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/LocatedFileStatus.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/LocatedFileStatus.java
@@ -202,4 +202,13 @@ public class LocatedFileStatus extends FileStatus {
   public int hashCode() {
     return super.hashCode();
   }
+
+  /**
+   * Compatible with Symlinks.
+   * @return
+   */
+  @Override
+  public boolean isFile() {
+    return super.isFile() || (isSymlink() && !isDirectory());
+  }
 }
