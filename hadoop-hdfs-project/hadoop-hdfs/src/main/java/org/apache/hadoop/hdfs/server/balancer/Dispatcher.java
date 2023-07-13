60bc999126a9dd9d7bc7f71159bd93c42606a761
jinbiao.sun <jinbiao.sun@shopee.com>
2023-07-13 14:52:35 +0800

    SPDI-95052. ZoneMover move replica failure related to rack reduction

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer/Dispatcher.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer/Dispatcher.java
@@ -317,6 +317,7 @@ public class Dispatcher {
               if (LOG.isDebugEnabled()) {
                 LOG.debug("Decided to move " + this);
               }
+              block.refreshLocations(source, target);
               return true;
             }
           }
@@ -602,10 +603,21 @@ public class Dispatcher {
 
   /** A class for keeping track of block locations in the dispatcher. */
   public static class DBlock extends MovedBlocks.Locations<StorageGroup> {
+    protected final List<StorageGroup> newLocations = new ArrayList<StorageGroup>(3);
     public DBlock(Block block) {
       super(block);
+      for (StorageGroup storageGroup: getLocations()) {
+        newLocations.add(storageGroup);
+      }
     }
 
+    public void refreshLocations(StorageGroup oldLoc, StorageGroup newLoc) {
+      newLocations.remove(oldLoc);
+      if (!newLocations.contains(newLoc)) {
+        newLocations.add(newLoc);
+      }
+    }
+    
     public long getNumBytes(StorageGroup storage) {
       return super.getNumBytes();
     }
@@ -1600,7 +1612,7 @@ public class Dispatcher {
      StorageGroup target, DBlock block) {
     List<DatanodeInfo> datanodeInfos = new ArrayList<>();
     synchronized (block) {
-      for (StorageGroup loc : block.locations) {
+      for (StorageGroup loc : block.newLocations) {
         datanodeInfos.add(loc.getDatanodeInfo());
       }
       datanodeInfos.add(target.getDatanodeInfo());
